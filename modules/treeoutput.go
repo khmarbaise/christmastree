314f47f671b024acdb4c7186da30729726759e4f
Karl Heinz Marbaise <kama@soebes.de>
2020-12-20 23:59:43 +0100

    Code improvement.

--- modules/treeoutput.go
+++ modules/treeoutput.go
@@ -1,37 +1,41 @@
 package modules
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 //TreeOutput create a christmastree output.
 func TreeOutput(numberOfLines int) (tree string) {
+
 	switch numberOfLines {
 	case 1:
 		tree =
-			"*"
+			strings.Repeat("*", numberOfLines*2-1)
 	case 2:
 		tree =
 			" *\n" +
-				"***"
+				strings.Repeat("*", numberOfLines*2-1)
 	case 3:
 		tree =
 			"  *\n" +
 				" ***\n" +
-				"*****"
+				strings.Repeat("*", numberOfLines*2-1)
 	case 4:
 		tree =
 			"   *\n" +
 				"  ***\n" +
 				" *****\n" +
-				"*******"
+				strings.Repeat("*", numberOfLines*2-1)
 	case 5:
 		tree =
 			"    *\n" +
 				"   ***\n" +
 				"  *****\n" +
 				" *******\n" +
-				"*********"
+				strings.Repeat("*", numberOfLines*2-1)
 	default:
 		fmt.Errorf("Unknown numberOfLines given. %d", numberOfLines)
 	}
-	return tree
+	return
 }
