03b2d2e2d93068a3c0364851f220dd4828f4024e
Karl Heinz Marbaise <kama@soebes.de>
2020-12-21 16:08:08 +0100

    Refactored code.

--- modules/treeoutput.go
+++ modules/treeoutput.go
@@ -11,23 +11,10 @@ func TreeOutput(numberOfLines int) (tree string) {
 	switch numberOfLines {
 	case 1:
 		tree = ""
-	case 2:
-		tree =
-			strings.Repeat(" ", numberOfLines-1) + "*\n"
-	case 3:
-		tree =
-			strings.Repeat(" ", numberOfLines-1) + strings.Repeat("*", 1) + "\n" +
-				strings.Repeat(" ", numberOfLines-2) + strings.Repeat("*", 3) + "\n"
-	case 4:
-		for i := 1; i < 4; i++ {
+	case 2, 3, 4, 5:
+		for i := 1; i < numberOfLines; i++ {
 			tree += strings.Repeat(" ", numberOfLines-i) + strings.Repeat("*", (i*2)-1) + "\n"
 		}
-	case 5:
-		tree =
-			strings.Repeat(" ", numberOfLines-1) + strings.Repeat("*", 1) + "\n" +
-				strings.Repeat(" ", numberOfLines-2) + strings.Repeat("*", 3) + "\n" +
-				strings.Repeat(" ", numberOfLines-3) + strings.Repeat("*", 5) + "\n" +
-				strings.Repeat(" ", numberOfLines-4) + strings.Repeat("*", 7) + "\n"
 	case 6:
 		tree =
 			strings.Repeat(" ", numberOfLines-1) + strings.Repeat("*", 1) + "\n" +
