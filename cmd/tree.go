c3a2d8c38477b35031f06dd21a0782205913ba29
Karl Heinz Marbaise <kama@soebes.de>
2020-12-22 19:49:15 +0100

    Improved code.

--- cmd/tree.go
+++ cmd/tree.go
@@ -6,16 +6,23 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+const numberOfLines = "number_of_lines"
+
 //CmdTree is test command.
 var CmdTree = cli.Command{
-	Name:        "tree",
-	Aliases:     []string{"tr"},
-	Usage:       "create a tree",
-	Description: `Create a tree`,
+	Name:    "tree",
+	Aliases: []string{"tr"},
+	Usage:   "Print out a christmas tree on console.",
+	Flags: []cli.Flag{
+		&cli.IntFlag{Name: numberOfLines, Aliases: []string{"nof"}, Value: 5},
+	},
+	Description: `Print a christmas tree on console with the given high (numberOfLines).`,
 	Action:      runEmpty,
 }
 
 func runEmpty(ctx *cli.Context) error {
-	fmt.Printf("%s\n", modules.TreeOutput(5))
+	var number = ctx.Int(numberOfLines)
+
+	fmt.Printf("%s\n", modules.TreeOutput(number))
 	return nil
 }
